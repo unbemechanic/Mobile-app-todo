b0d21ebe1f6b95aef317d83f622c897310cf6d46
Ilyosjon <the.mechanic99@bk.ru>
2024-10-11 19:46:11 +0900

    Mobile todo app

--- App.tsx
+++ App.tsx
@@ -5,114 +5,179 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
+  FlatList,
   Text,
-  useColorScheme,
   View,
+  TextInput,
+  Pressable,
 } from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import { Task } from './src/types/todo';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather'
+import Ant from 'react-native-vector-icons/AntDesign'
+import Entypo from 'react-native-vector-icons/Entypo'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+function generateKey(): string {
+  return `${Math.random().toString(8).substr(2, 9)}-${Date.now()}`
 }
-
+console.log(generateKey())
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<Set<string>>(new Set());
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleSubmit = () => {
+    if (task) {
+      setTasks([...tasks, { key: generateKey(), value: task }]);
+      setTask('');
+    }
+  };
+
+  const handleDelete = () => {
+    setTasks([]);
+    setCompletedTasks(new Set());
+  };
+
+  const handleDeleteItem = (taskKey: string) => {
+    setTasks(tasks.filter(task => task.key !== taskKey));
+    setCompletedTasks(new Set([...completedTasks].filter(key => key !== taskKey)));
+  };
+
+  const handleDecor = (taskKey: string) => {
+    const newCompletedTasks = new Set(completedTasks);
+    if (newCompletedTasks.has(taskKey)) {
+      newCompletedTasks.delete(taskKey);
+    } else {
+      newCompletedTasks.add(taskKey);
+    }
+    setCompletedTasks(newCompletedTasks);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <SafeAreaProvider>
+      <StatusBar barStyle={'light-content'} backgroundColor={''} />
+      <LinearGradient style={styles.linearGradient} colors={['#6a195961', '#8d3b9867', '#4c669f60']}>
+        <TextInput
+          placeholder="Add new task"
+          value={task}
+          onChangeText={setTask}
+          style={styles.input}
+        />
+        <Pressable onPress={handleSubmit}>
+          <Text style={styles.add}>Add</Text>
+        </Pressable>
+      </LinearGradient>
+      <LinearGradient colors={['#4c669fa9', '#8d3b9897', '#6a195988']} style={styles.container}>
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => (
+            <View style={{ width: '100%' }}>
+              <View style={styles.todos}>
+                <Text
+                  style={{
+                    textDecorationLine: completedTasks.has(item.key) ? 'line-through' : 'none',
+                    color: completedTasks.has(item.key) ? 'green' : 'black',
+                    fontSize: 20,
+                    paddingLeft: 20,
+                    flex: 3,
+                  }}
+                  key={item.key}
+                >
+                  {item.value}
+                </Text>
+                <Pressable onPress={() => handleDecor(item.key)}>
+                  {completedTasks.has(item.key) ? (
+                    <Icon size={34} style={{ color: 'green' }} name="check-circle" />
+                  ) : (
+                    <Entypo size={34} style={{ color: 'gray', marginRight: 33 }} name="circle" />
+                  )}
+                </Pressable>
+                <Pressable onPress={() => handleDeleteItem(item.key)}>
+                  {completedTasks.has(item.key) ? <Ant size={34} style={{ color: 'red' }} name="delete" /> : ''}
+                </Pressable>
+              </View>
+            </View>
+          )}
+        />
+        <Pressable
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            position: 'absolute',
+            left: 170,
+            bottom: 130,
+            backgroundColor: 'red',
+            padding: 20,
+            borderRadius: 10,
+          }}
+          onPress={handleDelete}
+        >
+          <Text style={{ color: 'white' }}>Delete All</Text>
+        </Pressable>
+      </LinearGradient>
+    </SafeAreaProvider>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+ linearGradient: {
+  height: 100,
+  width: '100%',
+  padding: 10,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent:'space-between',
+
+ },
+ input: {
+  width:'70%',
+  backgroundColor:'white',
+  borderRadius: 5,
+  borderColor:'white',
+  padding: 10,
+  height: 40
+ },
+ add: {
+  backgroundColor:'#0fe60f',
+  justifyContent:'center',
+  color:'#fff',
+  padding: 10,
+  height: 40,
+  paddingHorizontal: 20,
+  borderRadius: 5
+
+ },
+ container: {
+  height:'100%'
+ },
+ x: {
+  height: 30,
+  width: 30,
+  borderRadius:15,
+  backgroundColor:'red',
+  color:'#fff',
+  alignItems:'center',
+  textAlign:'center'
+ },
+ todos: {
+  height:60,
+  width:'90%',
+  marginHorizontal: 20,
+  flexDirection:'row',
+  backgroundColor:'white',
+  padding:10,
+  // justifyContent:'space-between',
+  marginTop: 20,
+  marginBottom:20,
+  borderRadius: 20,
+  alignItems: 'center'
+ }
 });
 
-export default App;
+
---
+++ src/types/todo.tsx
@@ -0,0 +1,4 @@
+export interface Task {
+    key: string;
+    value: string;
+}
